5ee4c42d462e8bb2b7cc8143a3495dfe21a3d152
aveid <tabyldieva1296@gmail.com>
2023-11-03 09:57:52 +0600

    split base.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path("", views.get_main_page),
+    path("", views.get_main_page, name="main_page"),
+    path("<int:pk>/", views.get_detail_blog, name="detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,8 @@ from blog.models import Blog
 def get_main_page(request):
     news = Blog.objects.all()
     return render(request, "blog/main_page.html", locals())
+
+def get_detail_blog(request, pk):
+    new = Blog.objects.get(id=pk)
+    return render(request, "blog/detail_page.html", locals())
+
